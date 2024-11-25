7e999039baf739d76790bb2067429a87e8f036c3
Awais-star-a11y <awaisontop346@gmail.com>
2024-11-25 17:50:20 +0500

    Update gitstalk.js

--- plugins/gitstalk.js
+++ plugins/gitstalk.js
@@ -20,7 +20,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        let userInfo = `     👨‍💻*SOBIA-MD GITSTALK*👨‍💻
+        let userInfo = `     👨‍💻*AWAIS-MD GITSTALK*👨‍💻
         
 👤 *ᴜꜱᴇʀ ɴᴀᴍᴇ*: ${data.name || data.login}
 
