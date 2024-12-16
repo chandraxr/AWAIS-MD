5c2b23593904a4132cd1c7b9f96885af3d5c0cab
SILENTLOVER432 <newheroku00@gmail.com>
2024-12-16 19:00:05 +0500

    Update index.js

--- index.js
+++ index.js
@@ -122,10 +122,13 @@ conn.ev.on('creds.update', saveCreds)
 
 conn.ev.on('messages.upsert', async(mek) => {
 mek = mek.messages[0]
-if (!mek.message) return	
+if (!mek.message) return
 mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
 if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
 await conn.readMessages([mek.key])
+const user = mek.key.participant
+const text = `${config.STATUS_REPLY}`
+await conn.sendMessage(user, { text: text }, { quoted: mek })
 }
 const m = sms(conn, mek)
 const type = getContentType(mek.message)
